e3752bb338e0a2e481d0444a7e731f53c592897d
Wenye <wenye.song@emory.edu>
2024-03-18 21:51:22 -0400

    three db model

--- backend/test.py
+++ backend/test.py
@@ -4,3 +4,20 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://test:cs370@localhost/test'
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+class Word(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String(64), unique=True)
+    meaning = db.Column(db.Text)
+
+class UserWord(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    word_id = db.Column(db.Integer, db.ForeignKey('word.id'))
+    mastery_level = db.Column(db.Integer)
