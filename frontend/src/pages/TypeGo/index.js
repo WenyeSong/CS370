cd1ee29083bb0d89413fceb5f5ecc82d901d8ede
chengyuShi <cshi46@emory.edu>
2024-04-04 17:16:24 -0400

    vocab test changed

--- frontend/src/pages/TypeGo/index.js
+++ frontend/src/pages/TypeGo/index.js
@@ -1,7 +1,88 @@
+import React, { useState, useEffect } from 'react'
+import './index.css'
+import randomWords, { generate } from 'random-words'
+import dictionary from './french-english.json';
+
+const NUM_OF_WORDS = 20
+
+
 function TypeGo () {
+  // words = what the app generates for quizzing the user
+  const [words, setWords] = useState([])
+  const [currInput, setCurrInput] = useState("")
+  const [currWordIndex, setCurrWordIndex] = useState(0)
+
+  const wrongWordList= []
+
+  useEffect(() => {
+    setWords(generateQuizList())
+  },[])
+
+  // the following function is derived from the tutorial: 
+  function generateQuizList() {
+    return new Array(NUM_OF_WORDS).fill(null).map(() => generate())
+  }
+
+  function handleKeyDown(event) {
+    console.log(event.key)
+  }
+
+
+
   return (
-    <div>TypeGo</div>
-  )
+    <div className = "TypeGo"> 
+      <div className="section">
+        <div className="is-size-1 has-text-centered has-text-primary">
+          <h2>TypeGo Quiz: Try Your Best!</h2>
+        </div>
+      </div>
+      <div className="control is-expanded section">
+        <input type="text" className="input" onKeyDown={handleKeyDown} value={currInput} 
+        onChange={(e) => setCurrInput(e.target.value)} />
+      </div>
+      <div className="section">
+        <button className="button is-info is-fullwidth">
+          Start
+        </button>
+      </div>
+      <div className = "section">
+         <div className="card">
+          <div className="card-content">
+            <div className="content">
+              {words.map((word, i) => (
+                <span key={i}>
+                  <span>
+                    {word.split("").map((char, idx) => (
+                      <span key = {idx}>{char}</span>
+                    ))}
+                  </span>
+                  <span> </span>
+                </span>
+              ))}
+            </div>
+          </div>
+         </div>
+      </div>
+    </div>
+  );
+
+  // function generateQuizList() {
+  //   const quizList = []
+
+  //   function formatAnswer(answerArray) {
+  //     return answerArray.join(', ');
+  //   }
+
+  //   for (let i = 0; i < NUM_OF_WORDS; i++) {
+  //     const randomIndex = Math.floor(Math.random() * Object.keys(dictionary).length);
+  //     const randomKey = Object.keys(dictionary)[randomIndex];
+  //     const question = randomKey;
+  //     const answer = formatAnswer(dictionary[randomKey]);
+
+  //     quizList.push({ id: i, question, answer });
+  //   }
+
+  // }
 }
 
-export default TypeGo
+export default TypeGo;
--- frontend/src/pages/Voctest/index.js
+++ frontend/src/pages/Voctest/index.js
@@ -7,7 +7,14 @@ function Voctest() {
   const [userChoice, setUserChoice] = useState('');
   const [vocabulary, setVocabulary] = useState([]);
   const [correctCount, setCorrectCount] = useState(0); // State to keep track of correct answers
-  const MAX_QUESTIONS = 30; // Max number of questions
+  const MAX_QUESTIONS = 2; // Max number of questions
+  let wrongWords = []; // keep track of wrong words
+
+  const [resultText, setResultText] = useState('');
+  const [feedbackText, setFeedbackText] = useState('');
+  const [feedbackButton, setFeedbackButton] = useState('Next');
+
+
 
   useEffect(() => {
     // Convert the object into an array of [key, value] pairs
@@ -22,21 +29,84 @@ function Voctest() {
     setVocabulary(limitedVocab);
   }, []);
 
-  const checkAnswer = () => {
+  // const checkAnswer = () => {
+  //   const nextQuestion = currentQuestion + 1;
+  //   if(nextQuestion <= MAX_QUESTIONS) {
+  //     const correctAnswer = vocabulary[currentQuestion][0]; // Get the key (term) of the current question
+  //     const isCorrect = correctAnswer.includes(userChoice); // Check if the user's choice matches any synonym
+  //     if (isCorrect) {
+  //       setCorrectCount((prevCount) => prevCount + 1); // Increment correct count if answer is correct
+  //       alert('Correct!');
+  //     } else {
+  //       wrongWords.push(vocabulary[currentQuestion]);
+  //       alert(`Wrong! The correct vocabulary is: ${correctAnswer}. Your input: ${userChoice}`); // Include user input and correct answer in the alert
+  //     }
+  //     // setCurrentQuestion((prevCurrent) => (prevCurrent + 1) % vocabulary.length);
+  //     // setUserChoice('');
+  //   }
+  //   else {
+  //     alert(`you have reached the end of quiz! Your correction rate is: ${correctCount}/${MAX_QUESTIONS}`)
+  //   }
+  // };
+
+  // called when submit is clicked
+  const showAnswer = () => {
+    // const resultLines = "";
+
     const correctAnswer = vocabulary[currentQuestion][0]; // Get the key (term) of the current question
-    const isCorrect = correctAnswer.includes(userChoice); // Check if the user's choice matches any synonym
-    if (isCorrect) {
-      setCorrectCount((prevCount) => prevCount + 1); // Increment correct count if answer is correct
-      alert('Correct!');
-    } else {
-      alert(`Wrong! The correct vocabulary is: ${correctAnswer}. Your input: ${userChoice}`); // Include user input and correct answer in the alert
+      const isCorrect = correctAnswer.includes(userChoice); // Check if the user's choice matches any synonym
+      if ((isCorrect) && !(userChoice === '')) {
+        setCorrectCount((prevCount) => prevCount + 1); // Increment correct count if answer is correct
+        setResultText('Correct!')
+      } 
+      else {
+        wrongWords.push(vocabulary[currentQuestion][0]);
+        setResultText(
+          `Wrong! The correct vocabulary is:
+          ${correctAnswer}.
+          Your input: ${userChoice}`)
+      }
+      if (currentQuestion + 1 === MAX_QUESTIONS) {
+        alert(`you have reached the end of quiz! Your correction rate is: ${correctCount}/${MAX_QUESTIONS}`)
+        // this comment will appear before the resultText gets updates and displayed. 
+        // setFeedbackText(`Accracy: ${Math.round(correctCount/MAX_QUESTIONS) * 100} %`);
+        
+        setFeedbackButton("check feedback on your vocab quiz");
+      }
+  
+    // document.getElementById("result-section").textContent = resultText;
+  }
+
+
+  function printArray(wrongWords) {
+    return wrongWords.join(', ')
+    // if (wrongWords.length === 0) {
+    //   return "there's no wrong words!"
+    // }
+    // else {
+    //   let output = ""
+    //   for (let i = 0; i < wrongWords.length; i++) {
+    //     output += wrongWords[i] + (i < wrongWords.length - 1 ? ", " : ""); // Add separator between words
+    //   }
+    //   return output;
+    // }
+  }
+
+  // called when next is clicked
+  const nextQuestion = () => {
+    
+    if (currentQuestion + 1 === MAX_QUESTIONS) {
+      setFeedbackText(
+        `Accuracy: ${correctCount/MAX_QUESTIONS}
+        Wrong Words: ${printArray(wrongWords)}`
+        );
     }
-    setCurrentQuestion((prevCurrent) => (prevCurrent + 1) % vocabulary.length);
-    setUserChoice('');
-  };
-  
-  
-  
+    else{
+      setCurrentQuestion((prevCurrent) => (prevCurrent + 1) % vocabulary.length);
+      setUserChoice('');
+      setResultText('');
+    }
+  }
 
   const handleChoiceChange = (event) => {
     setUserChoice(event.target.value);
@@ -71,10 +141,18 @@ function Voctest() {
             placeholder="Type a synonym here"
             className="definition-input"
           />
-          <button onClick={checkAnswer}>Submit</button>
+          {/* <button onClick={checkAnswer}>Submit</button> */}
+          <button onClick={showAnswer}>Submit</button>
         </div>
+
         <div className="result-section">
+          <p>{resultText}</p>
           <p>Correct Answers: {correctCount}</p>
+          <button onClick={nextQuestion}>{feedbackButton}</button> 
+        </div>
+        {/*the following section gives overall feedback and let the user see the wrong wordlist.*/}
+        <div className="feedback-section">
+          <p>{feedbackText}</p>
         </div>
       </header>
     </div>
