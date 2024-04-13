e4e8ada2e3bb7f9e819f7148bb4e40607a012557
cwan468 <cwan468@emory.edu>
2024-04-13 02:03:30 -0400

    french_dict update

---
+++ dictionary_crawl/french/filter_word.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+
+file_path = 'd:\\CS370\\dictionary_crawl\\french\\fra_wikipedia_2021_10K-words.txt'
+
+
+df = pd.read_csv(file_path, sep="\t", header=None, names=["Number", "Word", "Frequency"])
+
+def is_french_word(word):
+    for char in word:
+        if not ((char.isalpha() and char in "abcdefghijklmnopqrstuvwxyzàâäçéèêëîïôöùûüÿœæ") or char == "-"):
+            return False
+    return True
+
+
+filtered_df = df[df["Word"].apply(is_french_word)].drop(columns=['Number'])
+
+filtered_df.to_csv("d:\\CS370\\dictionary_crawl\\french\\filtered_words1.txt", sep="\t", index=False, header=False)
+
+print("Filtered data saved to 'filtered_words1.txt'.")
