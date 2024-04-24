dcfb6aaeed9c0274ff7679b53dd72fc75d971ee8
Leo-Fish <85286009+Leo-Fish@users.noreply.github.com>
2024-04-23 21:10:16 -0400

    Refactor get_random_words function to include print statements for debugging

--- backend/voctest.py
+++ backend/voctest.py
@@ -5,11 +5,6 @@ from sqlalchemy import func
 from db import db, User, Language, ForeignTerm, EnglishTranslation
 
 
-# async function getrandom(language_id) {
-#   const response = await fetch(`http://localhost:5000/user/words/${language_id}`, {method: 'GET'});
-#   let data = await response.json();
-#   console.log(data);
-# }
 
 def get_random_words(language_id=0):
     num_words = 3
@@ -18,6 +13,7 @@ def get_random_words(language_id=0):
     else:
         words = ForeignTerm.query.filter_by(language_id=language_id).order_by(func.random()).limit(num_words).all()
         if words:
+            print(words)
             return jsonify([word.term for word in words]), 200
         return jsonify({'message': 'No words found'}), 404
    
